1f9d224d99d729d2b753033a7199a991a1c4fe5b
bradenkwebb <bkwebb23@gmail.com>
2023-10-13 23:32:12 -0400

    my first working program in machine code!

--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -9,4 +9,36 @@
 // This program only needs to handle arguments that satisfy
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
-// Put your code here.
+@sum
+M=0         // sum = 0
+@i
+M=0         // i = 0
+
+(LOOP)      // this is where we check if i < R1
+@i
+D=M
+@R1
+D=D-M
+@BREAK
+D;JGE       // if i - R1 >= 0, jump to END
+
+@R0
+D=M
+@sum
+M=M+D       // sum = sum + R0
+
+@i
+M = M + 1   // i = i + 1
+
+@LOOP
+0;JMP
+
+(BREAK)
+@sum
+D=M
+@R2
+M=D         // R2 = sum
+
+(END)
+@END
+0;JMP
