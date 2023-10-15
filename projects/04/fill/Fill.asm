93cfb2d801585cc7a794ce69bcc0d21e1718ed2a
bradenkwebb <bkwebb23@gmail.com>
2023-10-15 00:35:05 -0400

    create program that reads keyboard input and outputs to screen

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -11,4 +11,73 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+
+// set the head of the array to the beginning of the screen
+@SCREEN
+D=A
+@arr
+M=D
+
+// set the length of the array to 8192
+@8192
+D=A
+@n
+M=D
+
+(LOOPBIG)
+    @i
+    M=0             // initialize i=0
+
+    @KBD            // read in the keyboard value
+    D=M
+    @BLACK
+    D;JNE
+
+    // Turn the entire screen white here
+    (WHITE)
+        // if i>=n, we're finished with the screen
+        @i
+        D=M
+        @n
+        D=D-M
+        @LOOPBIG
+        D;JGE
+
+        // set RAM[arr + i] = 0
+        @arr
+        D=M
+        @i
+        A=D+M
+        M=0
+
+        @i
+        M=M+1
+
+        @WHITE
+        0;JMP
+
+    (BLACK)
+        // we want to turn the entire screen black here
+        // if i>=n, we're finished with the screen
+        @i
+        D=M
+        @n
+        D=D-M
+        @LOOPBIG
+        D;JGE
+
+        // set RAM[arr + i] = -1
+        @arr
+        D=M
+        @i
+        A=D+M
+        M=-1
+
+        @i
+        M=M+1
+
+        @BLACK
+        0;JMP
+
+    @LOOPBIG
+    0;JMP
